779391810c3be3fb20c3868b96ed8c623138045f
Avelino Tiu <atiu@agsx.net>
2018-09-22 09:39:30 -0500

    implemented a custom affix function

--- src/_assets/js/scripts.js
+++ src/_assets/js/scripts.js
@@ -1,7 +1,31 @@
 $(document).ready(function() {
-  // $('.nav-slider').slick({
-  //   dots: false,
-  //   infinite: false, 
-  //   variableWidth: true
-  // });
+  var toggleAffix = function(affixElement, scrollElement, wrapper) {
+  
+    var height = affixElement.outerHeight(),
+        top = wrapper.offset().top;
+    
+    if (scrollElement.scrollTop() >= top){
+        wrapper.height(height);
+        affixElement.addClass("affix");
+    }
+    else {
+        affixElement.removeClass("affix");
+        wrapper.height('auto');
+    }
+      
+  };
+  
+
+  $('[data-toggle="affix"]').each(function() {
+    var ele = $(this),
+        wrapper = $('<div></div>');
+    
+    ele.before(wrapper);
+    $(window).on('scroll resize', function() {
+        toggleAffix(ele, $(this), wrapper);
+    });
+    
+    // init
+    toggleAffix(ele, $(window), wrapper);
+  });
 });
