838fa21ed3c0a785575f51a671423580dcb30295
Avelino Tiu <atiu@agsx.net>
2018-09-28 11:07:46 -0500

    make youtube embed play inside modal

--- dist/_assets/js/main.js
+++ dist/_assets/js/main.js
@@ -42,6 +42,8 @@ $(document).ready(function() {
 
   // end slick carousel
 
+
+  // hot links for the form modals
   if(window.location.href.indexOf('#whitepaper') != -1) {
     $('#whitepaperModal').modal('show');
   }
@@ -49,7 +51,30 @@ $(document).ready(function() {
   if(window.location.href.indexOf('#free-trial') != -1) {
     $('#freeTrialModal').modal('show');
   }
+  // end hotlinks
 
-  
+  // youtube embed in modal
+  var $videoSrc;  
+  $('.video-btn').click(function() {
+      $videoSrc = $(this).data( "src" );
+  });
+  console.log($videoSrc);
+
+    
+    
+  // when the modal is opened autoplay it  
+  $('#youtubeModal').on('shown.bs.modal', function (e) {
+      
+  // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+  $("#video").attr('src',$videoSrc + "?rel=0&amp;showinfo=0&amp;modestbranding=1&amp;autoplay=1" ); 
+  })
+    
+    
+  // stop playing the youtube video when I close the modal
+  $('#youtubeModal').on('hide.bs.modal', function (e) {
+      // a poor man's stop video
+      $("#video").attr('src',$videoSrc); 
+  }) 
+  // end youtube embed modal
 
 });
